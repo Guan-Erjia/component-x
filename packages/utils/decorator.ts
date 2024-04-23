5ddecb7347928eb48056ad04872a7390945b0fc0
guanjianzhou <guanjianzhou@baidu.com>
2024-04-23 19:09:11 +0800

    feat: 类型定义

--- packages/utils/decorator.ts
+++ packages/utils/decorator.ts
@@ -1,12 +1,18 @@
-export function XRegister(XComp: any) {
-  const name: string | undefined = XComp.name;
-  XComp.register = () => {
+export interface XComponent extends CustomElementConstructor {
+  name: string | undefined;
+  register: () => void;
+  callbackList?: Array<() => void>;
+}
+
+export function XRegister(target: XComponent) {
+  const name: string | undefined = target.name;
+  target.register = () => {
     if (!name) {
       return console.error("未定义组件名称");
     }
     if (customElements.get(name)) {
       console.warn(`自定义元素-- ${name} -- 重复注册！后注册的元素将会覆盖`);
     }
-    customElements.define(name, XComp);
+    customElements.define(name, target);
   };
 }
