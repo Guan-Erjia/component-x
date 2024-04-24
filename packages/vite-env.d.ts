b8b2fef5715239a1ceb440889b686a1bae3390bd
guanjianzhou <guanjianzhou@baidu.com>
2024-04-24 17:20:12 +0800

    fix: 添加声明

--- packages/vite-env.d.ts
+++ packages/vite-env.d.ts
@@ -6,6 +6,7 @@ declare module "*.vue" {
   export const XDialog;
   export const XButton;
   export const XCheckbox;
+  export const XCheckboxGroup;
   export const XRadio;
   export const XRadioGroup;
 }
